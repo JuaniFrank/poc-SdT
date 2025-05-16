bad538f70a09c9536af1e142a6b22537a27d6e07
Juan Frank <juanfrank.dev@gmail.com>
2025-05-15 22:23:19 -0300

    feat: create employee entity

---
+++ apps/api/src/employees/entities/employee.entity.ts
@@ -0,0 +1,14 @@
+export class Employee {
+    id: string;
+    name: string;
+    lastName: string;
+    email: string;
+    identityDocument: string;
+    birthDate: Date;
+    isDeveloper: boolean;
+    description: string;
+    areaId: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deleted: boolean;
+}
