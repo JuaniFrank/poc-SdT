a1e1eb1c931da515a95a4e138444c4678478b3be
Juan Frank <juanfrank.dev@gmail.com>
2025-05-16 19:57:46 -0300

    feat: create database module

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module, Global } from '@nestjs/common';
+import { createPool, Pool } from 'mysql2/promise';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: async (): Promise<Pool> => {
+        return createPool({
+          host: 'localhost',
+          port: 3307,
+          user: 'root',
+          password: '123456',
+          database: 'admin',
+          waitForConnections: true,
+          connectionLimit: 10,
+          queueLimit: 0,
+        });
+      },
+    },
+  ],
+  exports: ['DATABASE_CONNECTION'],
+})
+export class DatabaseModule {}
