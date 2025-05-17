5329ce9f47853864679d3137d240702b973988fd
Juan Frank <juanfrank.dev@gmail.com>
2025-05-17 19:24:49 -0300

    feat: define app routes

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,24 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DASHBOARD_ROUTES } from './dashboard/dashboard.routes';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-  {
     path: 'dashboard',
-    component: DashboardComponent
+    children: DASHBOARD_ROUTES,
   },
   {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  }
+    path: '**',
+    redirectTo: 'dashboard',
+  },
 ];
