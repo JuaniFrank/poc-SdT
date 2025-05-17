2a3c403c0d537105a3c85a0ee12f10d1f35745a3
Juan Frank <juanfrank.dev@gmail.com>
2025-05-17 19:20:58 -0300

    feat: create sidebar component

---
+++ apps/client/src/app/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {}
