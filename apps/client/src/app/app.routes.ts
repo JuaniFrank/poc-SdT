dc67598a79581a5ba12faa641cf716fa7dfabb77
Juan Frank <juanfrank.dev@gmail.com>
2025-05-17 18:12:10 -0300

    feat: define routes

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './public/login/login.component';
+import { RegisterComponent } from './public/register/register.component';
+import { DashboardComponent } from './private/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
