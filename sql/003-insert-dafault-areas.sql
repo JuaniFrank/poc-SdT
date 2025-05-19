ee5392215023dfb32f48703924a815ae5cddd423
Juan Frank <juanfrank.dev@gmail.com>
2025-05-19 14:29:23 -0300

    feat: add default values to areas

--- sql/003-insert-dafault-areas.sql
+++ sql/003-insert-dafault-areas.sql
@@ -1,7 +1,7 @@
-INSERT INTO areas (title) VALUES 
-  ('People & Culture'),
-  ('Software Development'),
-  ('Marketing'),
-  ('Sales');
+INSERT INTO areas (title, color) VALUES 
+  ('People & Culture', '#b00'),
+  ('Software Development', '#0b0'),
+  ('Marketing', '#00b'),
+  ('Sales', '#b0b');
   
   
