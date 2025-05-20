250032e22ded8f00ce7282fc76aa0df1ec670a92
Juan Frank <juanfrank.dev@gmail.com>
2025-05-20 19:30:07 -0300

    create user dtos

---
+++ apps/api/src/user/dto/update-user.dto.ts
@@ -0,0 +1,17 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  username?: string;
+}
