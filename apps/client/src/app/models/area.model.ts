a02245d164c465103134fcfdc3f3050a7c4cd903
Juan Frank <juanfrank.dev@gmail.com>
2025-05-20 14:44:12 -0300

    feat: create area model

--- apps/client/src/app/models/area.model.ts
+++ apps/client/src/app/models/area.model.ts
@@ -2,9 +2,23 @@ export class Area {
     id!: number;
     title!: string;
     color!: string;
-    quantity!: number;
 
     constructor(data: Partial<Area> = {}) {
         Object.assign(this, data);
     }
+}
+
+export class AreaQ extends Area {
+    quantity: number = 0;
+
+    constructor(data: Partial<AreaQ> = {}) {
+        super(data);
+        Object.assign(this, data);
+    }
+}
+
+export interface AreaIn {
+    id: number;
+    title: string;
+    color: string;
 }
