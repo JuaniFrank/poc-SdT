b5838a94dbc3060295bc7a201d81d7008b8da022
Juan Frank <juanfrank.dev@gmail.com>
2025-05-19 22:16:33 -0300

    feat: areas view

--- apps/client/src/app/dashboard/areas/areas.component.ts
+++ apps/client/src/app/dashboard/areas/areas.component.ts
@@ -1,30 +1,58 @@
 import { Component, inject, signal} from '@angular/core';
 import { AreaTableComponent } from '../shared/area-table/area-table.component';
-import { HttpClient } from '@angular/common/http';
 import { NgIf } from '@angular/common';
+import { AreaServiceTsService } from 'src/app/services/area/area.service';
+import { ButtonModule } from 'primeng/button';
+import { EditDialogComponent } from '../shared/edit-dialog/create-area-dialog.component';
+import { MessageModule } from 'primeng/message';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DialogService } from 'primeng/dynamicdialog';
+import { MessageService } from 'primeng/api';
+import { Area } from 'src/app/models/area.model';
 
 
 @Component({
   selector: 'app-areas',
   standalone: true,
-  imports: [AreaTableComponent, NgIf],
+  imports: [AreaTableComponent, NgIf, ButtonModule, MessageModule, DialogModule, DynamicDialogModule],
   templateUrl: './areas.component.html'
 })
 export class AreasComponent {
 
-  private http = inject(HttpClient);
+  private areaService = inject(AreaServiceTsService);
+
+  ref: DynamicDialogRef | undefined;
+
+  constructor(public dialogService: DialogService, public messageService: MessageService) {}
 
   loading = signal(true);
   areas = signal<any[]>([]);
 
-  ngOnInit() {
-    this.fetchAreas();
+  show() {
+    const header = 'New Area';
+
+    this.ref = this.dialogService.open(EditDialogComponent, {
+      header,
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true,
+    });
+
+    this.ref.onClose.subscribe((result: any) => {
+      if (result) {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Area created successfully' });
+        this.fetchAreasWithQuantitys();
+      }
+    });
   }
 
-  private fetchAreas() {
+  private fetchAreasWithQuantitys() {
     this.loading.set(true);
 
-    this.http.get<any[]>('http://localhost:3000/areas').subscribe({
+    this.areaService.getAreasWithQuantitys().subscribe({
       next: (data) => {
         this.areas.set(data);
         this.loading.set(false);
@@ -36,5 +64,44 @@ export class AreasComponent {
     });
   }
 
+  public updateAreaColor(area: any) {
+    this.areaService.update(area.id, { color: area.color }).subscribe({
+      next: () => {
+        this.fetchAreasWithQuantitys();
+      },
+      error: (err) => {
+        console.error('Error al actualizar color:', err);
+      }
+    });
+  }
 
+  public editArea(area: any) {
+    this.areaService.update(area.id, { title: area.title, color: area.color }).subscribe({
+      next: () => {
+        this.fetchAreasWithQuantitys();
+      },
+      error: (err) => {
+        console.error('Error al actualizar área:', err);
+      }
+    });
+    this.fetchAreasWithQuantitys();
+  }
+
+  public deleteArea(area: any) {
+    if (confirm('¿Estás seguro de que deseas eliminar esta área?')) {
+      this.areaService.delete(area.id).subscribe({
+        next: () => {
+          this.fetchAreasWithQuantitys();
+        },
+        error: (err) => {
+          console.error('Error al eliminar área:', err);
+        }
+      });
+    }
+    this.fetchAreasWithQuantitys();
+  }
+
+  ngOnInit() {
+    this.fetchAreasWithQuantitys();
+  }
 }
