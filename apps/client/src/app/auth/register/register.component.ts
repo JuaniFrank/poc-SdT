956e2fce26719fa44295f59e51cbd2ed96954220
Juan Frank <juanfrank.dev@gmail.com>
2025-05-20 21:36:25 -0300

    feat: create register form

--- apps/client/src/app/auth/register/register.component.ts
+++ apps/client/src/app/auth/register/register.component.ts
@@ -1,21 +1,57 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-  name = '';
-  email = '';
-  password = '';
+  private authService = inject(AuthService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
+
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('token') || '';
+
+      this.authService.isLoggedIn(token).subscribe((isLoggedIn) => {
+        if (isLoggedIn) {
+          this.router.navigate(['/dashboard']);
+        }
+      });
+    }
+  }
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    username: ['', Validators.required],
+  });
+
+  errorMessage = '';
 
   onSubmit() {
-    console.log('Registro:', { name: this.name, email: this.email });
+    console.log(this.loginForm.value)
+    if (this.loginForm.invalid) return;
+
+    const { email, password, username } = this.loginForm.value;
+
+    this.authService.register({ email, username, password }).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = error?.error?.message || 'Login failed. Please try again.';
+      },
+    });
   }
 }
